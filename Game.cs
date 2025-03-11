22026d0cb3f0a0bfaf982a305d7dea3450d60509
Blair Haines <Blair_Haines@outlook.com>
2025-03-10 20:08:06 -0400

    added Gravity and Jumping

--- Game.cs
+++ Game.cs
@@ -15,9 +15,16 @@ public class Game
     int currentFrame = 0; // Current Frame
     float frameTime = 5f; // Time per Frame
     float frameTimer = 0f; // Timer for Frame
+    //
+    int windowWidth = 500; // Window Width
+    int windowHeight = 500;// Window Height
 
-    int windowWidth = 500;
-    int windowHeight = 500;
+    //Gravity
+    float gravity = 0.5f;// Gravity
+    float jumpStrength = 10f;// Jump Strength
+    float yVelocity = 0f;// Y Velocity
+    bool isJumping = false;// Is Jumping
+    bool isGrounded = false;// Is Grounded
 
     /// <summary>
     ///     Setup runs once before the game loop begins.
@@ -27,7 +34,7 @@ public class Game
     {
         Window.SetTitle("Blair's 2D Game");//Set Title
         Window.SetSize(500, 500);//Set Window Size
-        Window.TargetFPS = 30;//Set Target FPS
+        Window.TargetFPS = 20;//Set Target FPS
     }
     /// <summary>
     ///     Update runs every frame.
@@ -50,23 +57,41 @@ public class Game
 
 
         {
-            if (Input.IsKeyboardKeyDown(KeyboardInput.Left))
+            if (Input.IsKeyboardKeyDown(KeyboardInput.Left))// If Left Arrow is Pressed
             {
-                playerX -= playerSpeed;
+                playerX -= playerSpeed;// Move Player Left
             }
-            else if (Input.IsKeyboardKeyDown(KeyboardInput.Right))
+            else if (Input.IsKeyboardKeyDown(KeyboardInput.Right))// If Right Arrow is Pressed
             {
-                playerX += playerSpeed;
+                playerX += playerSpeed;// Move Player Right
             }
-            else if (Input.IsKeyboardKeyDown(KeyboardInput.Up))
+            
+            if (isGrounded)
             {
-                playerY -= playerSpeed;
-            }
-            else if (Input.IsKeyboardKeyDown(KeyboardInput.Down))
-            {
-                playerY += playerSpeed;
-            }
+                if (Input.IsKeyboardKeyDown(KeyboardInput.Space))// If Space is Pressed
+                {
+                    
+                    
+                        yVelocity = -jumpStrength;// Add Jump Strength to Y Velocity
+                    isJumping = true;// Set isJumping to True
+                    isGrounded = false;// Set isGrounded to False
 
+                }
+            }
+            if (!isGrounded) // If Player is not Grounded
+            {
+                playerY += yVelocity; // Add Y Velocity to Player Y
+            }
+            {
+                yVelocity += gravity;// Add Gravity to Y Velocity
+            }
+            if (playerY >= 350)// If Player Y is greater than or equal to 350
+            {
+                playerY = 350;
+                isJumping = false;
+                isGrounded = true;
+                yVelocity = 0;
+            }
             //
             frameTimer += DeltaTime; // Add DeltaTime to FrameTimer
 
@@ -76,31 +101,31 @@ public class Game
                 currentFrame++; // Change Frame
                 
 
-                if (currentFrame > 3) 
+                if (currentFrame > 3) // If Current Frame is greater than 3
                 {
 
-                    currentFrame = 0;
+                    currentFrame = 0;// Reset Current Frame
                 }
                 // if player goes beyond the screen, wrap around
-                if (playerX > Window.Width)
+                if (playerX > Window.Width) // If Player X is greater than Window Width
                 {
-                    playerX = 0;
+                    playerX = 0; // Reset Player X
                 }
-                else if (playerX < 0)
+                else if (playerX < 0) // If Player X is less than 0
                 {
-                    playerX = Window.Width;
+                    playerX = Window.Width; // Reset Player X
                 }
                 Texture2D currentPlayerTexture = currentFrame switch// Switch Statement for Current Frame
                  { 
-                    0 => player, //
-                    1 => player2,
-                    2 => player3,
-                    3 => player4,
-                    _ => player
-                };
+                    0 => player, // Frame 1 
+                    1 => player2,// Frame 2
+                    2 => player3,// Frame 3
+                    3 => player4,// Frame 4
+                     _ => player // Default 
+                 };
 
                 Graphics.Draw(currentPlayerTexture, playerX, playerY);// Draw Player Image
-                Window.ClearBackground(Color.Clear);
+                Window.ClearBackground(Color.Clear);// Clear Background
 
 
             } }
