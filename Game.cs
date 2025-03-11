224cda93f62bad51668293bd15b34d2295b2517d
Blair Haines <Blair_Haines@outlook.com>
2025-03-11 00:08:49 -0400

    added background music

--- Game.cs
+++ Game.cs
@@ -1,6 +1,8 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
 using System.Numerics;
+
+
 // The namespace your code is in.
 namespace MohawkGame2D;
 /// <summary>
@@ -8,23 +10,26 @@ namespace MohawkGame2D;
 /// </summary>
 public class Game
 {
-    float playerX = 50;
-    float playerY = 350;
-    float playerSpeed = 7f;
-    float DeltaTime = 5f;
+    float playerX = 50; // Player X
+    float playerY = 350; // Player Y
+    float playerSpeed = 7f; // Player Speed
+    float DeltaTime = 5f; // Delta Time
     int currentFrame = 0; // Current Frame
-    float frameTime = 5f; // Time per Frame
+    float frameTime = 4f; // Time per Frame
     float frameTimer = 0f; // Timer for Frame
     //
     int windowWidth = 500; // Window Width
     int windowHeight = 500;// Window Height
 
     //Gravity
-    float gravity = 0.5f;// Gravity
-    float jumpStrength = 10f;// Jump Strength
-    float yVelocity = 0f;// Y Velocity
+    float gravity = 1.3f;// Gravity
+    float jumpStrength = 14f;// Jump Strength
+    float yVelocity = 1f;// Y Velocity
     bool isJumping = false;// Is Jumping
     bool isGrounded = false;// Is Grounded
+
+    //
+    
 
     /// <summary>
     ///     Setup runs once before the game loop begins.
@@ -34,7 +39,7 @@ public class Game
     {
         Window.SetTitle("Blair's 2D Game");//Set Title
         Window.SetSize(500, 500);//Set Window Size
-        Window.TargetFPS = 20;//Set Target FPS
+        Window.TargetFPS = 30;//Set Target FPS
     }
     /// <summary>
     ///     Update runs every frame.
@@ -54,16 +59,23 @@ public class Game
         Texture2D player2 = Graphics.LoadTexture("C:.\\bear-walk2.png");// Load Player Image
         Texture2D player3 = Graphics.LoadTexture("C:.\\bear-walk3.png");// Load Player Image
         Texture2D player4 = Graphics.LoadTexture("C:.\\bear-walk4.png");// Load Player Image
+        //
+        Music music = Audio.LoadMusic("C:.\\2D Game music.MP3");// Load Music
+
+
 
 
         {
+           bool isMoving = false; //Track if player is moving 
             if (Input.IsKeyboardKeyDown(KeyboardInput.Left))// If Left Arrow is Pressed
             {
                 playerX -= playerSpeed;// Move Player Left
+                isMoving = true;// player is moving 
             }
             else if (Input.IsKeyboardKeyDown(KeyboardInput.Right))// If Right Arrow is Pressed
             {
                 playerX += playerSpeed;// Move Player Right
+                isMoving = true; //player is moving 
             }
             
             if (isGrounded)
@@ -87,17 +99,19 @@ public class Game
             }
             if (playerY >= 350)// If Player Y is greater than or equal to 350
             {
-                playerY = 350;
-                isJumping = false;
-                isGrounded = true;
-                yVelocity = 0;
+                playerY = 350; // Reset Player Y
+                isJumping = false;// Set isJumping to False
+                isGrounded = true; // Set isGrounded to True
+                yVelocity = 0; // Reset Player Y
             }
             //
             frameTimer += DeltaTime; // Add DeltaTime to FrameTimer
 
-            if (frameTimer >= frameTime)
+            if (frameTimer >= frameTime) // If FrameTimer is greater than or equal to FrameTime
             {
                 frameTimer = 0; // Reset FrameTimer
+               
+
                 currentFrame++; // Change Frame
                 
 
@@ -123,9 +137,13 @@ public class Game
                     3 => player4,// Frame 4
                      _ => player // Default 
                  };
+                Music Sound = Audio.LoadMusic("C:.\\2D Game music.MP3");// Load Music not working wtf
 
                 Graphics.Draw(currentPlayerTexture, playerX, playerY);// Draw Player Image
+                                                                      //
+                Audio.Play(music);// Play Music
                 Window.ClearBackground(Color.Clear);// Clear Background
+               
 
 
             } }
